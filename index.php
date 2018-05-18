89d17f5b8f9e8ed5b27487af9ec46be1b5dff760
DavideL90 <you@example.com>
2018-05-18 17:00:52 +0200

    finito secondo punto

--- index.php
+++ index.php
@@ -11,22 +11,67 @@
       <div class="chart_cnt">
          <canvas id="myChartLine" width="800" height="600"></canvas>
       </div>
+      <div class="chart_cnt">
+         <canvas id="myChartLine2" width="800" height="600"></canvas>
+      </div>
+      <div class="chart_cnt">
+         <canvas id="myChartPie" width="800" height="600"></canvas>
+      </div>
 
       <script type="text/javascript">
          $(document).ready(function(){
             <?php include 'data.php'; ?>
-            var data = <?php echo json_encode($data); ?>;
 
+            // variabile per es 1
+            var data = <?php echo json_encode($data); ?>;
+            // grafico esercizio 1
             var ctx = document.getElementById('myChartLine').getContext('2d');
             var lineChart = new Chart(ctx, {
-               type: 'line',
+               type:'line',
                data: {
                   labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
                   datasets: [{
-                     label: 'Andamento vendite',
+                     label: 'Andamento vendite Esercizio 1',
                      data: data,
                      backgroundColor: 'rgba(255, 0, 0, 0.2)',
                      borderColor: 'orange'
+                  }]
+               }
+            });
+
+            // variabili per ex 2
+            var firstGraph = <?php echo json_encode($graphs['fatturato']); ?>;
+            console.log(firstGraph);
+            var secondGraph = <?php echo json_encode($graphs['fatturato_by_agent']); ?>;
+            console.log(secondGraph);
+            // grafici esercizio 2
+            var ctx1 = document.getElementById('myChartLine2').getContext('2d');
+            var lineChart = new Chart(ctx1, {
+               type: firstGraph.type,
+               data: {
+                  labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
+                  datasets: [{
+                     label: 'Andamento vendite Esercizio 2',
+                     data: firstGraph.data,
+                     backgroundColor: 'rgba(255, 0, 0, 0.2)',
+                     borderColor: 'orange'
+                  }]
+               }
+            });
+            var labels = [];
+            var values = [];
+            <?php foreach ($graphs['fatturato_by_agent']['data'] as $key => $value) { ?>
+               labels.push(<?php echo json_encode($key); ?>);
+               values.push(<?php echo json_encode($value); ?>);
+            <?php } ?>
+            var ctx2 = document.getElementById('myChartPie').getContext('2d');
+            var pieChart = new Chart(ctx2, {
+               type: secondGraph.type,
+               data: {
+                  labels: labels,
+                  datasets: [{
+                     data: values,
+                     backgroundColor: ['orange', 'blue', 'pink', 'yellow']
                   }]
                }
             })
